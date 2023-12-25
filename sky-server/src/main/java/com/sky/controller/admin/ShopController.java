9d6e9614a36cab8865d93e206431d83851d7aa66
Mentu <2621082160@qq.com>
2023-12-25 11:18:48 +0800

    功能实现(管理端设置店铺营业状态)

--- sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -6,10 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("adminShopController")
 @RequestMapping("/admin/shop")
@@ -33,5 +30,16 @@ public class ShopController {
         log.info("设置店铺的营业状态为：{}",status == 1 ? "营业中" : "打烊中");
         redisTemplate.opsForValue().set(KEY,status);
         return Result.success();
+    }
+    /**
+     * 获取店铺的营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取到店铺的营业状态为：{}",status == 1 ? "营业中" : "打烊中");
+        return Result.success(status);
     }
 }
